736475515fa64cd03e5ae8dfbc3016011447a954
Riccardo Santilli <riccardosantilli@live.it>
2024-09-25 16:31:59 +0200

    extra

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ const getInfo = () => {
       //manipolo il dom
       myObject.forEach((element) => {
         const card = document.createElement("div");
-
+        const totalP = document.createElement("p");
         const cardConteiner = document.querySelector(".row");
         card.classList.add("col-6", "col-md-4", "col-lg-3");
-        card.innerHTML = `<div class="card">
+        card.innerHTML = `<div class="card ">
             <img src="${element.img}" class="card-img-top" alt="img" id="apiImg" />
             <div class="card-body">
               <h5 class="card-title">${element.title}</h5>
@@ -30,6 +30,28 @@ const getInfo = () => {
         const scartBtn = card.querySelector(".btn-primary"); //seleziono il bottone della SINGOLA card
         scartBtn.addEventListener("click", () => {
           card.classList.add("d-none");
+        });
+        const buyBook = card.querySelector(".btn-success");
+        buyBook.addEventListener("click", () => {
+          const listItem = document.getElementById("bookList");
+          const singleItem = document.createElement("li");
+          singleItem.classList.add("list-group", "flex-row", "justify-content-between", "my-2");
+          singleItem.innerHTML = `<p class="m-0 border border-info px-3">
+          ${element.title} € <span class="price">${element.price}</span>
+          </p> <button class="btn btn-danger">Canc</button>`;
+          listItem.appendChild(singleItem);
+          let totalPrice = 0;
+          const total = document.querySelectorAll(".price");
+          total.forEach((price) => {
+            totalPrice += parseFloat(price.innerText);
+          });
+
+          const deleteBtn = singleItem.querySelector(".btn-danger");
+          deleteBtn.addEventListener("click", () => {
+            singleItem.classList.add("d-none");
+          });
+          totalP.innerText = `Il totale è: ${totalPrice}`;
+          listItem.appendChild(totalP);
         });
       });
     })
